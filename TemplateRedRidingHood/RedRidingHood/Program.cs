e48ca557c6ec86df64641590b3c135aca093447e
FireFlyForLife <maiko.s.99@gmail.com>
2019-01-07 18:05:34 +0100

    Added template project

---
+++ TemplateRedRidingHood/RedRidingHood/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Add code here:
+
+        Console.WriteLine("Hello Fellow Red Riding Hoods!");
+
+        Console.ReadKey();
+    }
+}
