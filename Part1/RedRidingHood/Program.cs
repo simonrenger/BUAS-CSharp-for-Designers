ea6211d314127212013b6f032c27d0b119e4ccc0
FireFlyForLife <maiko.s.99@gmail.com>
2019-01-13 15:00:37 +0100

    Minified code, Fixed a bug where not the whole input stream buffer was being read

--- Part1/RedRidingHood/Program.cs
+++ Part1/RedRidingHood/Program.cs
@@ -21,69 +21,61 @@ class Program
         *  ======================================================================================================================================================================
         *  ======================================================================================================================================================================
         */
-        // initial menu print:
+
+        //Initial print
         Console.WriteLine("Welcome to *Red Riding Hood* what do you want to do? \n\n");
-        Console.WriteLine("1. Play Game");
-        Console.WriteLine("2. View Credits");
-        Console.WriteLine("3. Quit Game");
-        Console.Write("\n>");
+
+        //Save last key press in a variable
         ConsoleKeyInfo key = new ConsoleKeyInfo();
         while (key.Key != ConsoleKey.Escape)
         {
+            //Ask player for which option they want
+            Console.WriteLine("1. Play Game");
+            Console.WriteLine("2. View Credits");
+            Console.WriteLine("3. Quit Game");
+            Console.Write("\n>");
+
             key = Console.ReadKey();
             Console.WriteLine("\n");
-            if (key.Key == ConsoleKey.D1)
+
+            if (key.Key == ConsoleKey.D1) //When "Key 1" has been pressed
             {
                 Console.Clear();
                 Console.WriteLine("What shall be the game name?");
                 Console.Write("\n>");
                 string name = Console.In.ReadLine();
-                Console.WriteLine("The name of the game is {0}",name);
-                Console.WriteLine("\n\n1. Play Game");
-                Console.WriteLine("2. View Credits");
-                Console.WriteLine("3. Quit Game");
-                Console.Write("\n>");
+                Console.WriteLine("The name of the game is {0}\n",name);
             }
-            else if (key.Key == ConsoleKey.D2)
+            else if (key.Key == ConsoleKey.D2) //When "Key 2" has been pressed
             {
                 Console.Clear();
                 Console.WriteLine("~~~~~~~~~ Credits ~~~~~~~~~~");
                 Console.WriteLine("Programmers:");
                 Console.WriteLine("Simon ");
                 Console.WriteLine("Maiko");
-                Console.WriteLine("\n\n1. Play Game");
-                Console.WriteLine("2. View Credits");
-                Console.WriteLine("3. Quit Game");
-                Console.Write("\n>");
+                Console.WriteLine("\n\n");
             }
-            else if (key.Key == ConsoleKey.D3)
+            else if (key.Key == ConsoleKey.D3) //When "Key 3" has been pressed
             {
                 Console.Clear();
                 Console.WriteLine("Do you want to quit the game?");
                 Console.WriteLine("(Y/N)");
                 Console.Write("\n>");
-                int decision = Console.Read();
-                if (decision == 'Y')
+                ConsoleKey decision = Console.ReadKey().Key;
+                if (decision == ConsoleKey.Y)
                 {
                     Environment.Exit(1);
                 }
                 else
                 {
+                    //We decided not to quit, so clear console and go back up again
                     Console.Clear();
-                    Console.WriteLine("1. Play Game");
-                    Console.WriteLine("2. View Credits");
-                    Console.WriteLine("3. Quit Game");
-                    Console.Write("\n>");
                 }
             }
-            else
+            else //When you pressed anything else than key 1, 2 or 3
             {
                 Console.Clear();
-                Console.WriteLine("Your entry was not correct at all!");
-                Console.WriteLine("1. Play Game");
-                Console.WriteLine("2. View Credits");
-                Console.WriteLine("3. Quit Game");
-                Console.Write("\n>");
+                Console.WriteLine("Your entry was not correct at all!\n");
             }
         }
 
