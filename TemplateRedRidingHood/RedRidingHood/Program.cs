ab5175173dbfa5696d4f5f4b80c18325c3a7c3b3
FireFlyForLife <maiko.s.99@gmail.com>
2019-02-20 09:10:28 +0100

    added constants

--- TemplateRedRidingHood/RedRidingHood/Program.cs
+++ TemplateRedRidingHood/RedRidingHood/Program.cs
@@ -4,11 +4,48 @@
 
 class Program
 {
+    static string[] IntroLines = new string[]
+    {
+        "Once upon a time, there was a little girl who lived in a village near the forest.  Whenever she went out, the little girl wore a red riding cloak, so everyone in the village called her Little Red Riding Hood.",
+        "One morning, Little Red Riding Hood asked her mother if she could go to visit her grandmother as it had been awhile since they'd seen each other.",
+        "\"That's a good idea,\" her mother said.  So they packed a nice basket for Little Red Riding Hood to take to her grandmother.",
+        "When the basket was ready, the little girl put on her red cloak and kissed her mother goodbye.",
+        "Remember, go straight to Grandma's house,\" her mother cautioned.  \"Don't dawdle along the way and please don't talk to strangers!  The woods are dangerous.\"",
+        "\"Don't worry, mommy,\" said Little Red Riding Hood, \"I'll be careful.\""
+    };
+
+
+    /*
+     * Text for the environment Exercise 
+     *
+     *  Key:                     Value:
+     * "Rock to my left",       "Woaw what a big rock!"
+     * "River over there",      "It can’t even move the big rock!"
+     * "Orchid near my feet",   "It smells really nice!"
+     * 
+     */
+
+    /*
+     * Text for the custom class Exercise
+     *
+     *  Key:                     Value:
+     *                          Color:      height:     mass:       isLiquid:
+     * "Rock to my left",       Gray,       5.0f,       150.0f,     false
+     * "River over there",      Blue,       1.5f,       2500.0f,    true
+     * "Orchid near my feet",   Purple,     0.2f,       0.4f,       false
+     * 
+     */
+
     static void Main(string[] args)
     {
         //Add code here:
+        
 
-        Console.WriteLine("Hello Fellow Red Riding Hoods!");
+
+        
+
+
+
 
         Console.ReadKey();
     }
