65a0bee6c44324abfe218e8c76d3c34aac579362
FireFlyForLife <maiko.s.99@gmail.com>
2019-04-03 09:09:39 +0200

    Added unity day 3

---
+++ Unity 3/Assets/Scripts/BasicPlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class BasicPlayerMovement : MonoBehaviour
+{
+    private Transform tr;
+
+    public GameObject prefab;
+
+    // Lets test it!
+    public float MovementSpeed = 2.5f;
+
+    void Awake()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        
+        Debug.Log(gameObject.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hor = Input.GetAxisRaw("Horizontal");
+        float vert = Input.GetAxisRaw("Vertical");
+
+        Vector3 worldPosition = transform.position;
+        worldPosition += new Vector3(hor, 0, vert) * MovementSpeed;
+        transform.position = worldPosition;
+
+        if (Input.GetButton("Jump"))
+        {
+            GameObject newPlayer = Instantiate(prefab, transform.position + new Vector3(2, 0, 0), Quaternion.identity);
+            newPlayer.GetComponent<BasicPlayerMovement>().prefab = prefab;
+
+        }
+    }
+}
---
+++ Unity 3/Assets/Scripts/MouseShootComponent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseShootComponent : MonoBehaviour
+{
+    public Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 forward = transform.forward;
+
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        
+        if( Physics.Raycast(ray, out hit))
+        {
+            Debug.DrawLine(transform.position, hit.point, Color.black, 0.1f);
+        }
+
+    }
+}
